168093e267e13a8cf6fa8bb36dcfecaf14a778d9
Dylan Phelan <dtphelan1@gmail.com>
2020-10-17 15:51:20 -0400

    Forget file reorg; focus on api

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from model_details import luce_dirichlet
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+API_BASE_URL = 'api'
+VERSION = 'v1'
+PORT = '1080'
+
+# VARIABLES FOR RCV SIMULATION
+# TODO: THESE NEED TO BE MADE INTO INPUT PARAMS FROM THE WEBFORM
+##
+poc_share = 0.30
+poc_support_for_poc_candidates = 0.66
+poc_support_for_white_candidates = 0.34
+white_support_for_white_candidates = 1-1e-3
+white_support_for_poc_candidates = 1e-3
+num_ballots = 30
+num_simulations = 1
+seats_open = 6
+num_poc_candidates = 6
+num_white_candidates = 6
+max_ballot_length = 6
+# Luce model (Dirichlet variation)
+concentrations = [0.5]*4  # >>1 means very similar supports, <<1 means most support goes to one or two candidates
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('num_ballots', type=int, required=True)
+# TODO: ADD location once hooked up to a webform ,(location='form')
+
+
+class RCVSimulation(Resource):
+    def post(self):
+        args = parser.parse_args()
+        poc_elected_rcv, poc_elected_atlarge = luce_dirichlet(
+            poc_share=poc_share,
+            poc_support_for_poc_candidates=poc_support_for_poc_candidates,
+            poc_support_for_white_candidates=poc_support_for_white_candidates,
+            white_support_for_white_candidates=white_support_for_white_candidates,
+            white_support_for_poc_candidates=white_support_for_poc_candidates,
+            num_ballots=args['num_ballots'],
+            num_simulations=num_simulations,
+            seats_open=seats_open,
+            num_poc_candidates=num_poc_candidates,
+            num_white_candidates=num_white_candidates,
+            concentrations=concentrations
+        )
+        print("poc_elected_atlarge", poc_elected_atlarge)
+        print("poc_elected_rcv", poc_elected_rcv)
+        return dict({'results': poc_elected_rcv})
+
+
+print(os.path.join('', API_BASE_URL, VERSION, 'simulate'))
+api.add_resource(RCVSimulation, '/' + os.path.join(API_BASE_URL, VERSION, 'simulate'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=PORT)
